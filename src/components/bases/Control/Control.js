e0a2bdae51f51034b5e14a44a399da5d115a1df1
paraparata <srizalmp@gmail.com>
2021-04-02 00:16:16 +0700

    add control section

---
+++ src/components/bases/Control/Control.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Control.module.scss";
+
+import Button from "../../ui/Button/Button";
+
+const pads = [];
+
+for (let i = 0; i < 8; i++) {
+  pads.push(i);
+}
+
+export default function Control() {
+  return (
+    <div className={styles.control}>
+      {pads.map((pad) => {
+        return <Button key={pad} variant="full" />;
+      })}
+    </div>
+  );
+}
--- src/pages/Drumpads.js
+++ src/pages/Drumpads.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../components/ui/Button/Button";
+import Control from "../components/bases/Control/Control";
 import Grid from "../components/ui/Grid/Grid";
 import Header from "../components/bases/Header/Header";
 
@@ -31,6 +32,33 @@ export default function Drumpads() {
         <hr />
       </div>
       <div style={{ flex: 1, padding: "0.5rem" }}>
+        <div
+          style={{
+            marginBottom: "12px",
+            height: "30px",
+            display: "flex",
+            gap: "0.5rem",
+          }}
+        >
+          <Button variant="blok">Play</Button>
+          <Button variant="blok">Record</Button>
+        </div>
+        <Control />
+        <div
+          style={{
+            marginTop: "12px",
+            height: "30px",
+            display: "flex",
+            gap: "0.5rem",
+          }}
+        >
+          <Button variant="blok">Keys</Button>
+          <Button variant="blok">ClearNote</Button>
+          <Button variant="blok">ClearAll</Button>
+          <Button variant="blok">Bars</Button>
+        </div>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
         <Grid>
           {pads.map((pad) => {
             return <Button key={pad} variant="full" />;
